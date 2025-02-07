406dbed65b396fe657e4e382d6732e02cd232a5f
Anthony <ants52@aol.com>
2025-02-07 10:12:37 +0700

    v4.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [4.2.2] - 2025-02-07
+
+* Dependency updates
+
 ## [4.2.1] - 2025-01-05
 
 * Dependency updates
--- lib/wp_json_api.dart
+++ lib/wp_json_api.dart
@@ -23,7 +23,7 @@ import '/models/wp_user.dart';
 import '/networking/network_manager.dart';
 
 /// The version of the wp_json_api
-String _wpJsonAPIVersion = "4.2.1";
+String _wpJsonAPIVersion = "4.2.2";
 
 /// The base class to initialize and use WPJsonAPI
 class WPJsonAPI {
